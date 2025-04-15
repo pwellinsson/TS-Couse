4dde7ef7b81a1c58d52683c1c0f14c65551415e0
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2025-04-15 17:49:53 -0300

    Create and Finalize intro

---
+++ Course_freeCode/01Intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Peter", lastName: "Parker", age: 22, email: "peterson@me.com" };
+console.log("Hello World");
+console.log(user.name);
---
+++ Course_freeCode/01Intro/intro.ts
@@ -0,0 +1,4 @@
+let user = {name: "Peter", lastName: "Parker", age: 22, email: "peterson@me.com"}
+
+console.log("Hello World")
+console.log(user.name)

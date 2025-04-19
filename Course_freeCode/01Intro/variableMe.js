6b0916d6f2872fbe5f3a17c587534a6b4eac36fe
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2025-04-19 16:43:29 -0300

    Update variableMe.js

--- Course_freeCode/01Intro/variableMe.js
+++ Course_freeCode/01Intro/variableMe.js
@@ -1,2 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// string
 var greeting = "Hello World";
+// num.toUpperCase(); ERROR. num não é uma string
+greeting.toLowerCase();
 console.log(greeting);
+// number
+var userId = 10.43456;
+// boolean
+var hasLogin = false;
+// any
+// let hero;
+var hero;
+function getHero() {
+    return "thor";
+}
+hero = getHero();
